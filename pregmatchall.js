b3d04c13b265ca16acb352b87a8b2ef28d4915af
Camille Hodoul <camille.hodoul@gmail.com>
2014-06-26 22:12:52 +0200

    First version of the function and tests.

---
+++ pregmatchall.js
@@ -0,0 +1,55 @@
+/*
+* Want preg_match_all in JS ? use this, although it returns the array of matches instead of using a reference to a local variable.
+* One more thing : I named the function "js*Preg*MatchAll, but it isn't magic : regex implementation varies from PHP's PCRE to JS.
+* Please let met know if you find it usefull or if you know how to improve it (especially the offset part) !
+* https://github.com/Eartz/pregMatchAll.js
+* camille.hodoul [cat] gmail.com
+*/
+function jsPregMatchAll(pattern,s,flag,offset) {
+	var order = flag || "PREG_PATTERN_ORDER";
+	var matches = [];
+	
+	if(typeof(offset)!=="undefined" && offset>0) {
+		// try to reproduce the behavior of the offset parameter, but I'm not sure how to test it.
+		// I have to rebuild a pattern.
+		var ps = pattern.toString();
+		var delimiter = ps.charAt(0);
+		/*
+		* FIXME : If the user has escaped his delimiter in the pattern, I should unescape it before passing it to the RegExp constructor.
+		* but this should be done after the .join() ofc
+		*/
+		var t = ps.split(delimiter); 
+        t.shift();
+		var flags = t.pop();
+		t[0] = ".{"+offset+"}"+t[0];
+		ps = t.join(delimiter);
+		
+		pattern = new RegExp(ps,flags); // Have to rebuild it at runtime so no literal...
+		
+	}
+	s.replace(pattern,function(){
+			var args = [].slice.call(arguments);
+			// Remove unnecessary elements from the args array
+			args.pop();
+			var offset = args.pop();
+			var substr = args[0];
+			// args now only contains the matches
+			if(order==="PREG_SET_ORDER") {
+			  matches.push(args);
+			}
+			else if(order==="PREG_PATTERN_ORDER") {
+			  var l = args.length;
+			  if(!matches[0]) matches[0] = [];
+			  matches[0].push(substr);
+			  for(var i=1;i<l;i++) {
+				  if(!matches[(i+1)]) matches[(i+1)] = [];
+				  matches[(i+1)].push(args[i]);
+			  }
+			}
+			else if(order==="PREG_OFFSET_CAPTURE") {
+				matches.push([substr,offset]);
+			}
+	});
+	return matches;
+}
+
---
+++ test.php
@@ -0,0 +1,89 @@
+<?php
+$tests = array(
+	0 => array(
+		"pattern" => "/ *([a-zA-Z0-9\-]+) *: *([^;]*)/",
+		"subject" => "X-MyHeader: MyValue; X-AZE: adqdsdfff;USERAGENT: Chrome123123 é'",
+		"flag" => "PREG_SET_ORDER",
+		"offset" => false
+	),
+	1 => array(
+		"pattern" => "/^def/",
+		"subject" => "abcdef",
+		"flag" => "PREG_OFFSET_CAPTURE",
+		"offset" => 3
+	),
+	2 => array(
+		"pattern" => "/def/",
+		"subject" => "abcdef",
+		"flag" => "PREG_PATTERN_ORDER",
+		"offset" => false
+	)
+);
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Tests</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<style>
+			table tr td {
+				border:solid #C0C0C0 1px;
+				max-width:900px;
+				overflow-x:auto;
+			}
+			table tr th {
+				font-weight:bold;
+				border : solid black 1px;
+			}
+		</style>
+    </head>
+    <body>
+		<script type="text/javascript" src="pregmatchall.js"></script>
+		<table cellspacing="0">
+			<tr>
+				<th>#</th>
+				<th>PHP $matches (json encoded)</th>
+				<th>JS matches (.toString())</th>
+				<th>Same ?</th>
+			</tr>
+		<?php
+	
+		foreach($tests as $n => $test):
+			$flag = constant($test['flag']);
+			if($test['offset']!==false) {
+				preg_match_all($test['pattern']."s",$test['subject'],$matches,$flag,$test['offset']);
+			}
+			else {
+				preg_match_all($test['pattern']."s",$test['subject'],$matches,$flag);
+			}
+			?>
+			<tr>
+				<td><?php echo $n; ?></td>
+				<td><pre id="php_output_<?php echo $n; ?>"><?php echo json_encode($matches); ?></pre></td>
+				<td><pre id="js_output_<?php echo $n; ?>"></pre></td>
+			</tr>
+			<?php
+		endforeach;
+		?>
+		</table>
+		<script type="text/javascript">
+			
+			document.addEventListener("DOMContentLoaded",function() {
+				<?php // i'm doing this because I want to test with the RegExp literal
+					  // I'm not using <?= tags because my local server cfg sucks.
+					foreach($tests as $n => $test):
+						?>
+						var matches_<?php echo $n; ?> = jsPregMatchAll(<?php echo $test['pattern']."gm"; ?>,"<?php echo $test['subject']; ?>","<?php echo $test['flag']; ?>"<?php if($test['offset']) echo ",".$test['offset']; ?>);
+					   
+						document.getElementById("js_output_<?php echo $n; ?>").innerHTML = JSON.stringify(matches_<?php echo $n; ?>);
+						
+						<?php
+					endforeach;
+				?>
+			},false);
+		</script>
+	</body>
+</html>
